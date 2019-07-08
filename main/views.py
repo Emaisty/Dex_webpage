7a1a515ce1ec0a1980192fa09fdd562e97a4a300
Emaisty <maxgusevfff@yandex.ru>
2019-07-08 21:25:05 +0300

    create login and register pages and func in views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
 
 
 def index(request):
-    content = {1: 1}
-    return render(request, 'index.html', content)
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    else:
+        return redirect('/login')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        data = request.POST.dict()
+        # check
+        return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        data = request.POST.dict()
+        # create
+        return render(request, 'index.html')
