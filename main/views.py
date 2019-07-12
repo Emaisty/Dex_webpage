d9faca4e525ce0ac2d42182e1b1d1cca4302aa77
Emaisty <maxgusevfff@yandex.ru>
2019-07-13 02:27:17 +0300

    create different content to auth users and anon users

--- main/views.py
+++ main/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_protect
 
 
 def index(request):
-    if request.user.is_authenticated:
-        return render(request, 'index.html')
-    else:
-        return redirect('/log_in')
+    return render(request, 'index.html', {'auth': request.user.is_authenticated})
 
 
+@csrf_protect
 def log_in(request):
-    if request.method == "GET":
-        return render(request, 'log_in.html')
-    else:
-        data = request.POST.dict()
-        user = authenticate(username=data['name'], password=data['password'])
-        login(request, user)
-        return redirect('/')
+    data = request.POST.dict()
+    user = authenticate(username=data['name'], password=data['password'])
+    login(request, user)
+    return redirect('/')
+
+
+@csrf_protect
+def log_out(request):
+    logout(request)
+    return redirect('/')
 
 
 def register(request):
